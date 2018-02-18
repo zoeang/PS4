d6ca7b15c09a453562ca41a547e3bc0dd11c43a8
zoeang <36051346+zoeang@users.noreply.github.com>
2018-02-18 16:16:55 -0600

    Moving on Part 1
    Created a class, set the validity, and tested the class.
    
    Potential issue with creating the class. See line 59

--- AngPS4.R
+++ AngPS4.R
@@ -1,4 +1,4 @@
-#Getting Started
+#Getting Started-------------------------------------------------------------------
 montyhall<-function(pick){ #I took away the second door argument because the user shouldn't be allowed to decide that
   pick<-sample(1:3, 1)
   car<-sample(1:3, 1)
@@ -12,3 +12,63 @@ montyhall<-function(pick){ #I took away the second door argument because the use
 montyhall()
 # Should return a TRUE if these samples are equal and
 # a false if they are not
+
+#Moving On-----------------------------------------------------------------------
+
+#Create new S4 class door
+
+#Constructor Function
+setClass(Class="door",
+         representation = representation(
+           chosenDoor="numeric", #class numeric
+           carDoor="numeric",  #class numeric
+           switch="logical"  #class logical
+         ),
+         prototype= prototype(
+           chosenDoor=sample(1:3,1), #data for the items
+           carDoor=sample(1:3,1),
+           switch=sample(c(T,F), 1)
+         )
+)
+
+#Set validity
+
+setValidity("door", function(x){
+  testChosen<-any(x@chosenDoor==1 | x@chosenDoor==2 |x@chosenDoor==3) #object must be 1,2, or 3
+  testCar<-any(x@carDoor==1 | x@carDoor==2 |x@carDoor==3)
+  testSwitch<-any(x@switch==T | x@switch==F)
+  if(testChosen==F){
+    print("chosenDoor value not valid")
+  }
+  if(testCar==F){
+    print("carDoor value not valid")
+  }
+  if(testSwitch==F){
+    print("switch must be a logical")
+  }
+}
+)
+
+#Do this because reasons
+setMethod("initialize", "door", function(.Object, ...){
+  value=callNextMethod() 
+  validObject(value)
+  return(value)
+})
+
+new("door") ##########When I set the class, this will "randomly" create the same data each time.
+            # If I reset the class, different data will repeat
+
+new("door", chosenDoor="m", carDoor="w", switch="P") #test the representation ; this will throw an error 
+new("door", chosenDoor=9, carDoor="w", switch=T) #test the validity; this will throw an error
+new("door", chosenDoor=2, carDoor=4, switch=T) #test the validity; this will throw an error
+new("door", chosenDoor=2, carDoor=1, switch="A") #test the representation; this will throw an error
+
+
+
+
+
+
+
+
+
